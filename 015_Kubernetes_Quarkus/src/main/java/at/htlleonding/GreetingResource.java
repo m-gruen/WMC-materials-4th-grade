88f0e147a3f3a1dca0d3e603d670a83aeb54ca62
Mark Grünzweil <mark.gruenzweil@gmail.com>
2026-03-09 08:31:35 +0100

    feat: Enhance GreetingResource to include multiple greeting messages from properties, ConfigMap, and secret

--- 015_Kubernetes_Quarkus/src/main/java/at/htlleonding/GreetingResource.java
+++ 015_Kubernetes_Quarkus/src/main/java/at/htlleonding/GreetingResource.java
@@ -1,16 +1,30 @@
 package at.htlleonding;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
-public class GreetingResource {
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
-    }
+@Path("/hello")  
+public class GreetingResource {  
+  
+    @ConfigProperty(name = "greeting.message.properties", defaultValue="(missing)")  
+    String greetingMessageProperties;  
+  
+    @ConfigProperty(name = "greeting.message.configmap", defaultValue="(missing)")  
+    String greetingMessageConfigMap;  
+  
+    @ConfigProperty(name = "greeting.message.secret", defaultValue="(missing)")  
+    String greetingMessageSecret;  
+  
+    @GET  
+    @Produces(MediaType.TEXT_PLAIN)  
+    public String hello() {  
+        return "Hello from Quarkus REST" +  
+                "\nProperties: " + greetingMessageProperties +  
+                "\nConfigMap: " + greetingMessageConfigMap +  
+                "\nSecret: " + greetingMessageSecret;  
+    }  
 }
